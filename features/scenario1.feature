a8b82a77945e636f96822588ad43e23263319200
Kevin McDermott <bigkevmcd@gmail.com>
2022-02-17 21:09:17 +0000

    End of session commit.

--- features/scenario1.feature
+++ features/scenario1.feature
@@ -3,3 +3,36 @@ Feature: Monopoly Game
     Given player "A" on "The Angel Islington"
     When the player rolls a 2 and 1
     Then they land on "Pentonville Road"
+
+  Scenario: Player Movement
+    Given player "B" on "The Angel Islington"
+    When the player rolls a 6 and 3
+    Then they land on "Pall Mall"
+
+  Scenario: Passing through GO!
+    Given player "B" on "Park Lane"
+    And player "B" has a balance of £1000
+    When the player rolls a 1 and 3
+    Then they land on "Old Kent Road"
+    And their balance is now £1200
+    
+
+  Scenario: Passing through GO!
+    Given player "B" on "Park Lane"
+    And player "B" has a balance of £1000
+    When the player rolls a 1 and 2
+    Then they land on "Go"
+    And their balance is now £1200
+
+  Scenario: Passing through GO!
+    Given player "B" on "Go"
+    And player "B" has a balance of £1000
+    When the player rolls a 4 and 2
+    Then they land on "The Angel Islington"
+    And their balance is now £1000
+
+  Scenario: Go to Jail
+    Given player "C" on "Leicester Square"
+    When the player rolls a 3 and 1
+    Then they are moved immediately to "In Jail"
+    And they do not pass through Go
--- features/steps/steps.js
+++ features/steps/steps.js
@@ -1,1 +1,89 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then, Before } = require('@cucumber/cucumber');
+const assert = require('assert');
+
+// https://www.monopolyland.com/uk-monopoly-board-list-of-properties/
+
+var board = [
+    {name: "Go"},
+    {name: "Old Kent Road"},
+    null,
+    null,
+    null,
+    null,
+    {name: "The Angel Islington"},
+    null,
+    {name: "In Jail"},
+    {name: "Pentonville Road"},
+    {name: "Leicester Square"},
+    null,
+    null,
+    null,
+    {name: "Goto Jail"},
+    {name: "Pall Mall"},
+    {name: "Park Lane"},
+    null,
+    null
+];
+
+var game;
+var currentPlayer;
+var passedGo;
+
+Before(() => {
+    passedGo = false;
+    currentPlayer = null;
+    game = {
+        "positions": {
+        },
+        "balances": {
+        },
+        "in-jail": {
+        }
+    }
+});
+
+const jailIndex = board.findIndex((where) => where?.name === "In Jail");
+const gotoJailIndex = board.findIndex((where) => where?.name === "Goto Jail");
+
+Given('player {string} on {string}', function (player, location) {
+    const position = board.findIndex((where) => where?.name === location);
+    currentPlayer = player;
+    game.positions[currentPlayer] = position;
+});
+
+When('the player rolls a {int} and {int}', function (d1, d2) {
+    const total = d1 + d2;
+    let currentPosition = game.positions[currentPlayer];
+    currentPosition += total;
+    if (currentPosition === gotoJailIndex) {
+        currentPosition = jailIndex;
+        game['in-jail'][currentPlayer] = true;
+    }
+
+    if (currentPosition >= board.length) {
+        game.balances[currentPlayer] += 200;
+        passedGo = true;
+    }
+    game.positions[currentPlayer] = currentPosition % board.length;
+});
+
+Then('they land on {string}', function (expectedLocation) {
+    const currentLocation = board[game.positions[currentPlayer]].name;
+    assert.equal(currentLocation, expectedLocation);
+});
+
+Given('player {string} has a balance of £{int}', function (player, balance) {
+    currentPlayer = player;
+    game.balances[currentPlayer] = balance;
+});
+
+Then('their balance is now £{int}', function (expectedBalance) {
+    assert.equal(game.balances[currentPlayer], expectedBalance);
+});
+
+Then('they are moved immediately to {string}', function (string) {
+});
+
+Then('they do not pass through Go', function () {
+    assert.equal(passedGo, false);
+});
